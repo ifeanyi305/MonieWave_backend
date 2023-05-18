dfdc2ee1a3e37902674fcb793058e3799a399b32
V-Blaze <valentine6586@gmail.com>
2023-05-18 09:58:14 +0100

    Add exchange rate routes to routes.rb file

--- app/controllers/api/v1/exchange_rate_controller.rb
+++ app/controllers/api/v1/exchange_rate_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::ExchangeRateController < ApplicationController
   require 'date'
-  skip_before_action :authenticate_request, only: %i[eur_rate]
+  skip_before_action :authenticate_request, only: %i[get_last_currency_rate get_all_last_rate  get_all_rates_data]
   before_action :check_admin, only: %i[create]
 
+  # Endpoint to create a new exchange rate
   def create
     @exchange_rate = ExchangeRate.new(rate_params)
     @exchange_rate.time = DateTime.now
@@ -16,6 +17,7 @@ class Api::V1::ExchangeRateController < ApplicationController
     end
   end
 
+  # Endpoint to get latest exchange rate for a given currency
   def get_last_currency_rate
     @currency_rate = ExchangeRate.where(currency: params[:currency]).order(created_at: :desc).first
 
@@ -26,6 +28,7 @@ class Api::V1::ExchangeRateController < ApplicationController
     end
   end
 
+  # Endpoint to get latest exchange rate for euro and pounds
   def get_all_last_rate
     @euro_rate = ExchangeRate.where(currency: 'euro').order(created_at: :desc).first
     @pounds_rate = ExchangeRate.where(currency: 'pounds').order(created_at: :desc).first
@@ -37,6 +40,7 @@ class Api::V1::ExchangeRateController < ApplicationController
     end
   end
 
+  # Endpoint to get the history data for euro and pounds
   def get_all_rates_data
     euro_rates = ExchangeRate.where(currency: 'euro')
     pounds_rates = ExchangeRate.where(currency: 'pounds')
@@ -47,7 +51,8 @@ class Api::V1::ExchangeRateController < ApplicationController
     end
   end
 
-  
+  private
+
   def rate_params
     params.require(:data).permit(:currency, :price)
   end
