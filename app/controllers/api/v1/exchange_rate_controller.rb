e5845f6015dd83003354319738e34e11777b1e20
V-Blaze <valentine6586@gmail.com>
2023-05-18 09:37:07 +0100

    Add method for getting exchange rate of a given param

--- app/controllers/api/v1/exchange_rate_controller.rb
+++ app/controllers/api/v1/exchange_rate_controller.rb
@@ -3,16 +3,6 @@ class Api::V1::ExchangeRateController < ApplicationController
   skip_before_action :authenticate_request, only: %i[eur_rate]
   before_action :check_admin, only: %i[create]
 
-  def get_last_euro_rate
-    @euro_rate = ExchangeRate.where(currency: 'euro').order(created_at: :desc).first
-
-    if @euro_rate
-      render json: {message: 'Latest rate', data: @eur_rate }, status: :ok
-    else
-      return render json: { message: "No exchange rate found for euro." }, status: :not_found
-    end
-  end
-
   def create
     @exchange_rate = ExchangeRate.new(rate_params)
     @exchange_rate.time = DateTime.now
@@ -25,6 +15,28 @@ class Api::V1::ExchangeRateController < ApplicationController
       return render json: { message: "Exchange rate update failed" }, status: :unprocessable_entity
     end
   end
+
+  def get_last_currency_rate
+    @currency_rate = ExchangeRate.where(currency: params[:currency]).order(created_at: :desc).first
+
+    if @currency_rate
+      render json: {message: "Latest #{params[:currency]} rate", data: @currency_rate }, status: :ok
+    else
+      return render json: { message: "No exchange rate found for #{params[:currency]}." }, status: :not_found
+    end
+  end
+
+  def get_all_last_rate
+    @euro_rate = ExchangeRate.where(currency: 'euro').order(created_at: :desc).first
+    @pounds_rate = ExchangeRate.where(currency: 'pounds').order(created_at: :desc).first
+
+    if @euro_rate && @pounds_rate
+      render json: {message: 'Latest rates', data: {Euro: @euro_rate, Pounds: @pounds_rate } }, status: :ok
+    else
+      return render json: { message: "No exchange rate found." }, status: :not_found
+    end
+  end
+
   
   def rate_params
     params.require(:data).permit(:currency, :price)
