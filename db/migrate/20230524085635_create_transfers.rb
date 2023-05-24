768bcc0cbbb37f7490deb64bb310a46c2c36a94f
V-Blaze <valentine6586@gmail.com>
2023-05-24 10:05:20 +0100

    Generate model and migration for transfer

---
+++ db/migrate/20230524085635_create_transfers.rb
@@ -0,0 +1,21 @@
+class CreateTransfers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :transfers do |t|
+      t.string :currency
+      t.integer :amount
+      t.integer :naira_amount
+      t.integer :exchange_rate
+      t.string :recipient_name
+      t.integer :recipient_account
+      t.string :recipient_bank
+      t.string :recipient_phone
+      t.string :reference_number
+      t.string :payment_method
+      t.string :status
+
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
