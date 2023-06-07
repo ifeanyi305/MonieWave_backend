a671f3f9c18ae7f5def2e8c12bdb2260695c9aef
V-Blaze <valentine6586@gmail.com>
2023-06-07 22:32:48 +0100

    Update users Migration table

--- db/migrate/20230504233505_create_users.rb
+++ db/migrate/20230504233505_create_users.rb
@@ -7,6 +7,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :email
       t.boolean :verified
       t.string :role
+      t.string :status
+      t.string :last_login_month
       t.string :password_digest
       t.string :reset_password_token, null: true
       t.timestamp :reset_password_sent_at, null: true
