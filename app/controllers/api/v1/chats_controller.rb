dca75655675e0115942dbd6a10ea61dd0085d94c
V-Blaze <valentine6586@gmail.com>
2023-07-05 13:47:56 +0100

    Implement chat index method

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -1,4 +1,21 @@
 class Api::V1::ChatsController < ApplicationController
+  before_action :check_admin, only: %i[index]
+
+  def index
+    @chats = Chat.where(admin_id: @current_user.id).includes(:user, :messages)
+    chat_data = @chats.map do |chat|
+      {
+        
+        user_details: { chat_id: chat.id, time: chat.updated_at, user_id: chat.user_id, email: chat.user.email, name: chat.user.first_name }
+      }
+    end
+    render json: { chats: chat_data }, status: :ok
+  end
+  
+  
+  
+  
+
   def create
     if params[:data][:sender] == 'admin'
       @chat = Chat.find_by(user_id: params[:data][:user_id])
